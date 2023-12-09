255fcecfcf1034d0fd74009baa370ac8c2b411dd
DileshMKaria <143887186+DileshMKaria@users.noreply.github.com>
2023-12-09 12:52:50 +0530

    DESIGNED UI FOR LOGIN AND SIGNUP

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginAndSignUp from './components/Login&SignUp/LoginAndSignUp';
 import Cards from './components/Cards.js/Cards';
+import Login from './Login/Login';
+
 
 function App() {
   return (
-    <Router>
-      <div style={{ backgroundColor: 'white' }}>
-        <Routes>
-        <Route exact path="/*" element={<LoginAndSignUp/>} />
-        <Route path="/cards" element={<Cards/>} />
-        {/* Add more Route components for other routes */}
-        </Routes>
-      </div>
-    </Router>
+    // <Router>
+    //   <div style={{ backgroundColor: 'white' }}>
+    //     <Routes>
+    //     <Route exact path="/*" element={<LoginAndSignUp/>} />
+    //     <Route path="/cards" element={<Cards/>} />
+    //     {/* Add more Route components for other routes */}
+    //     </Routes>
+    //   </div>
+    // </Router>
+    <Login/>
+    
   );
 }
 
